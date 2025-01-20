6d2af7f7d1dfde12b1de5cec483079b8c76f7722
Ilya Zverev <ilya@zverev.info>
2025-01-20 17:34:28 +0200

    Generate social images

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     baseUrl: "izv.ee",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
